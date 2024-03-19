651e86af369e1528a72613fe555cc27367df1e31
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2024-03-19 15:32:22 -0400

    Import test crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 #[cfg(test)]
 mod test {
+    use uno::one;
+    use two::two;
+    use workspace_test::three;
+
+    #[test]
+    fn test_one_two() {
+        assert_eq!(one() + two(), three());
+    }
+
     #[ignore]
     #[test]
     fn test_solidity_compatibility_ipa() {
