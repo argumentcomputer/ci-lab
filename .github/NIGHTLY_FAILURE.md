d34e7db25a3a54b9e26d299b95782c3b3c5da9b3
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-02-10 16:49:35 -0500

    Cleanup

--- .github/NIGHTLY_FAILURE.md
+++
@@ -1,7 +0,0 @@
----
-title: "Nightly M1 Mac test run failed"
-labels: automated issue
----
-M1 Mac tests failed in {{ env.WORKFLOW_URL }}
-Commit: {{ env.GIT_COMMIT }}
-Commit date: {{ env.GIT_COMMIT_DATE }}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
 #[cfg(test)]
 mod test {
-    use uno::one;
-    use two::two;
-    use workspace_test::three;
-
     #[test]
-    fn test_one_two() {
-        assert_eq!(one() + two(), three());
-    }
-
-    #[ignore]
-    #[test]
-    fn test_solidity_compatibility_ipa() {
-        //panic!()
+    fn it_works() {
+        assert_eq!(1 + 1, 2);
     }
 }
